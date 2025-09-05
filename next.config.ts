df6b0f6ea812caef55af4e707c78e6b854d1bd63
Sopan Patil <patilsopan4148@gmail.com>
2025-09-05 18:22:48 +0000

    https://github.com/sopan-hub/my-image-assets/blob/main/file_000000001eb0

--- next.config.ts
+++ next.config.ts
@@ -40,6 +40,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
