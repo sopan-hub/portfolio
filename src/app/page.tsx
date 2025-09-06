b8783272df4ed8317ef72135e7e7a258989a1043
Sopan Patil <patilsopan4148@gmail.com>
2025-09-06 05:43:14 +0000

    can you add in teh background a something far is glowing a in the blue c

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ import Footer from '@/components/layout/footer';
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col">
+    <div className="flex min-h-screen flex-col radial-glow">
       <Header />
       <main className="flex-1">
         <HeroSection />
