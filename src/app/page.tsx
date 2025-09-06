b1b85075e8db7dbd20f851e34922ff4122c372cb
Sopan Patil <patilsopan4148@gmail.com>
2025-09-06 06:21:41 +0000

    remove all the bacground

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ import Footer from '@/components/layout/footer';
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col body-bg">
+    <div className="flex min-h-screen flex-col">
       <Header />
       <main className="flex-1">
         <HeroSection />
