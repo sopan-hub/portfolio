2fddf071dcadc8fc88d00a532654575968c1c66e
Sopan Patil <patilsopan4148@gmail.com>
2025-09-07 10:06:48 +0000

    can you mka ethat about me in the squere shape abd big big

--- src/components/sections/about.tsx
+++ src/components/sections/about.tsx
@@ -6,7 +6,7 @@ import UiverseCard from '@/components/uiverse-card';
 const AboutSection = () => {
   return (
     <section id="about" className="py-20">
-      <div className="container md:w-3/4 lg:w-2/3">
+      <div className="container md:w-3/4 lg:w-1/2">
         <motion.div
           initial={{ opacity: 0, y: 50 }}
           whileInView={{ opacity: 1, y: 0 }}
