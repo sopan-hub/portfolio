38c12477afd3171b4187675a0a38c787fefa8398
Sopan Patil <patilsopan4148@gmail.com>
2025-09-07 09:46:20 +0000

    <!-- From Uiverse.io by adamgiebl --> <button class="neu-button">Press

--- src/components/uiverse-card.tsx
+++ src/components/uiverse-card.tsx
@@ -2,11 +2,8 @@ import React from 'react';
 
 const UiverseCard = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="uiverse-parent">
-      <div className="uiverse-card">
-        <div className="uiverse-glass"></div>
-        <div className="uiverse-content">{children}</div>
-      </div>
+    <div className="neu-card">
+      {children}
     </div>
   );
 };
