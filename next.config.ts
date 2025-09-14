a81e69f271b74c362e211d35b5ffbcd4f6641dae
Sopan Patil <patilsopan4148@gmail.com>
2025-09-14 16:23:18 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -43,7 +43,6 @@ const nextConfig: NextConfig = {
     ],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    timeout: 60000,
   },
 };
 
