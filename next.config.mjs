8fc01bf53b835eb25c4bc2bf88d586be40594e10
Sopan Patil <patilsopan4148@gmail.com>
2026-05-07 14:39:23 +0000

    site is looking like this

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,20 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  devIndicators: {
-    buildActivity: true,
-    buildActivityPosition: 'bottom-right',
-  },
-  experimental: {
-    allowedDevOrigins: [
-      'https://6000-firebase-studio-1757078021811.cluster-52r6vzs3ujeoctkkxpjif3x34a.cloudworkstations.dev',
-    ],
-  },
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'raw.githubusercontent.com',
-        port: '',
-        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
       },
     ],
+  },
+  experimental: {
+    allowedDevOrigins: ["*"],
   },
 };
 
